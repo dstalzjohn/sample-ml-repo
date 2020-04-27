878a5545aa05641a0785c1a2b7290d7f7587793c
Denis Stalz-John <denis.stalz-john@codecentric.de>
2020-04-27 16:24:45 +0200

    added logger

--- samplemlproject/callbacks/callback_factories.py
+++ samplemlproject/callbacks/callback_factories.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from keras.callbacks.callbacks import ModelCheckpoint
+from keras.callbacks.callbacks import ModelCheckpoint, CSVLogger
 
 from samplemlproject.config.envconfig import get_run_id, RUN_ID_KEY
 
@@ -10,3 +10,10 @@ def model_callback_factory(filepath: str, *args, **kwargs):
     p = Path(filepath)
     p.parent.mkdir(parents=True, exist_ok=True)
     return ModelCheckpoint(filepath=filepath, *args, **kwargs)
+
+
+def logging_callback_output(filename: str, *args, **kwargs):
+    filename = filename.replace("$" + RUN_ID_KEY, get_run_id())
+    p = Path(filename)
+    p.parent.mkdir(parents=True, exist_ok=True)
+    return CSVLogger(filename=filename, *args, **kwargs)
