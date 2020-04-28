3ab17da18a5474c06cd7935bcc0f435f9aa531d6
Denis Stalz-John <denis.stalz-john@codecentric.de>
2020-04-28 14:07:54 +0200

    added logging to ccml callback

--- samplemlproject/callbacks/ccmlprojectcallback.py
+++ samplemlproject/callbacks/ccmlprojectcallback.py
@@ -1,8 +1,8 @@
 from os.path import join
-from typing import List
+from typing import List, Optional
 
 import yaml
-from keras.callbacks import Callback
+from keras.callbacks import Callback, CSVLogger
 
 from samplemlproject.callbacks.gitversioncallback import produce_git_version_yaml
 from samplemlproject.config.envconfig import get_run_id, get_short_id
@@ -18,14 +18,34 @@ class CCMLProjectCallback(Callback):
 
     def __init__(self, log_path: str, git_dirs: List[str] = [], git_modules: List[str] = [],
                  git_version_filename: str = "git_versions.yml",
-                 project_info_filename: str = "project_info.yml"):
+                 project_info_filename: str = "project_info.yml",
+                 enable_train_logging: bool = True,
+                 train_log_file: str = "train_logs.csv"):
         super().__init__()
         self.git_dirs: List[str] = [git_dirs] if type(git_dirs) is str else git_dirs
         self.git_modules: List[str] = [git_modules] if type(git_modules) is str else git_modules
         self.log_path = log_path
         self.git_version_filename: str = git_version_filename
         self.project_info_filename: str = project_info_filename
+        self.log_callback: Optional[Callback] = None
+        if enable_train_logging:
+            self.log_callback = CSVLogger(filename=join(log_path, train_log_file))
+
+    def set_model(self, model):
+        super().set_model(model)
+        if self.log_callback is not None:
+            self.log_callback.set_model(model)
 
     def on_train_begin(self, logs=None):
         produce_git_version_yaml(join(self.log_path, self.git_version_filename), self.git_dirs, self.git_modules)
         produce_project_info(join(self.log_path, self.project_info_filename))
+        if self.log_callback is not None:
+            self.log_callback.on_train_begin(logs=logs)
+
+    def on_epoch_end(self, epoch, logs=None):
+        if self.log_callback is not None:
+            self.log_callback.on_epoch_end(epoch, logs=logs)
+
+    def on_train_end(self, logs=None):
+        if self.log_callback is not None:
+            self.log_callback.on_train_end(logs=logs)
