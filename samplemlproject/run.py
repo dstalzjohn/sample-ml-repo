64a1c7f059ac0113850f5357f5de631334993e8e
Denis Stalz-John <denis.stalz-john@codecentric.de>
2020-05-04 15:13:08 +0200

    working on documentation

--- samplemlproject/run.py
+++ samplemlproject/run.py
@@ -1,31 +1,3 @@
-# Copyright 2020 QuantumBlack Visual Analytics Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
-# NONINFRINGEMENT. IN NO EVENT WILL THE LICENSOR OR OTHER CONTRIBUTORS
-# BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-# The QuantumBlack Visual Analytics Limited ("QuantumBlack") name and logo
-# (either separately or in combination, "QuantumBlack Trademarks") are
-# trademarks of QuantumBlack. The License does not grant you any right or
-# license to the QuantumBlack Trademarks. You may not use the QuantumBlack
-# Trademarks or any confusingly similar mark as a trademark for your product,
-#     or use the QuantumBlack Trademarks in any other manner that might cause
-# confusion in the marketplace, including but not limited to in advertising,
-# on websites, or on software.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Application entry point."""
 from pathlib import Path
 from typing import Dict, Union
@@ -55,8 +27,8 @@ class ProjectContext(KedroContext):
         os.environ[RUN_ID_KEY] = self._local_run_id
         os.environ[SHORT_ID_KEY] = self._local_short_id
 
-    project_name = "kedro-sample"
-    project_version = "0.15.9"
+    project_name = "samplemlproject"
+    project_version = "0.0.1"
 
     def _get_run_id(
         self, *args, **kwargs  # pylint: disable=unused-argument
